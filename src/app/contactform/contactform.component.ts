67b73cd143d0e4ed4fe7a7bb4a4babfe1355b763
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-08-19 21:45:28 +0200

    contactform error

--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
@@ -11,15 +12,52 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ContactformComponent {
   isChecked: boolean = false;
+  checkboxTouched: boolean = false;
   contactData = {
     name: '',
   email: '',
   message: ''
   }
+  mailTest = true;
+  http = inject(HttpClient);
 
-  onSubmit(ngForm: NgForm){
-    if(ngForm.valid && ngForm.submitted){
-    console.log(this.contactData);
+  // post = {
+  //   endPoint: 'https://deineDomain.de/sendMail.php',
+  //   body: (payload: any) => JSON.stringify(payload),
+  //   options: {
+  //     headers: {
+  //       'Content-Type': 'text/plain',
+  //       responseType: 'text',
+  //     },
+  //   },
+  // };
+
+  //  onSubmit(ngForm: NgForm) {
+    // if (ngForm.submitted && ngForm.valid && !this.mailTest) {
+    //   this.http.post(this.post.endPoint, this.post.body(this.contactData))
+    //     .subscribe({
+    //       next: (response) => {
+
+    //         ngForm.resetForm();
+    //       },
+    //       error: (error) => {
+    //         console.error(error);
+    //       },
+    //       complete: () => console.info('send post complete'),
+    //     });
+    // } else if (ngForm.submitted && ngForm.valid && this.mailTest) {
+
+    //   ngForm.resetForm();
+    // }
+  //  }
+
+   onSubmit(ngForm: NgForm){
+    if (ngForm.submitted && ngForm.valid){
+      console.log(this.contactData);
     }
+   }
+
+  validateCheckbox() {
+    this.checkboxTouched = true;
   }
 }
